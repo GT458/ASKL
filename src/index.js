ea58cd325ad4a4b90ec24ef84ff79778bda261a2
GT458 <isohrob1@gmail.com>
2021-07-27 14:47:28 -0400

    add keyHandler

--- src/index.js
+++ src/index.js
@@ -1,23 +1,82 @@
 const Input = require('./scripts/input')
-import * as keyInputs from './scripts/key_input';
+import keyInputs from './scripts/key_input';
+
 import * as THREE from './scripts/three.module.js';
-  let camera, scene, renderer;
-  let geometry, material, mesh;
+let camera, scene, renderer;
+let geometry, material, mesh;
 
 document.addEventListener("DOMContentLoaded", () => {
-  
+  const keyHandler = new keyInputs();
+  window.addEventListener("keydown", function(event) {
+      // debugger;
+      if (event.defaultPrevented) {
+        return;
+      }
+     
+        switch(event.key) {
+          case 'a':
+            keyHandler.setKeyDown('a');
+            // setKeyDown('a');
+            // this.keysDown.a = true;
+            break;
+          case 's':
+            keyHandler.setKeyDown('s');
+            // keysDown.s = true;
+            break;
+          case 'k':
+            keyHandler.setKeyDown('k');
+            // keysDown.k = true;
+            break;
+          case 'l':
+            keyHandler.setKeyDown('l');
+            // keysDown.l = true;
+            break;
+          default:
+            console.log('key down');
+        }
+      
+        
+  })
+
+  window.addEventListener("keyup", function(event) {
+      if (event.defaultPrevented) {
+        return;
+      }
+
+      switch(event.key) {
+        case 'a':
+            keyHandler.setKeyDown('a');
+            
+            break;
+          case 's':
+            keyHandler.setKeyDown('s');
+            // keysDown.s = true;
+            break;
+          case 'k':
+            keyHandler.setKeyDown('k');
+            // keysDown.k = true;
+            break;
+          case 'l':
+            keyHandler.setKeyDown('l');
+            // keysDown.l = true;
+            break;
+        default:
+          console.log('key up')
+      }
+    })
+  console.log(keyHandler.getKeyDown());
   // console.log('loaded');
   // const input = new Input(document.querySelector('.button'), document.querySelector('span'));
   // const canvas = document.querySelector('#gameCanvas');
   // const gl = canvas.getContext("webgl2");
-
+  
   camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	camera.position.z = 1;
+	camera.position.z = 2;
 
 	scene = new THREE.Scene();
 
 	geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-	material = new THREE.MeshNormalMaterial();
+	material = new THREE.MeshBasicMaterial({color: "green"});
 
 	mesh = new THREE.Mesh( geometry, material );
 	scene.add( mesh );
@@ -26,6 +85,9 @@ document.addEventListener("DOMContentLoaded", () => {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animation );
 	document.body.appendChild( renderer.domElement );
+  if (keyHandler.keysDown.a) {
+    console.log('a was pressed!')
+  }
   // animate();
   
   // if (gl === null) {
@@ -39,8 +101,9 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 function animation(time) {
     // requestAnimationFrame(animate);
-    mesh.rotation.x = Date.now() * 0.00005;
-    mesh.rotation.y = Date.now() * 0.0001;
+    // mesh.position.x = keyInputs.keysDown.a ? mesh.position.x + 1 : 0;
+    // mesh.rotation.x = Date.now() * 0.0005;
+    // mesh.rotation.y = Date.now() * 0.001;
     renderer.render( scene, camera );
   }
 function main() {
--- src/scripts/key_input.js
+++ src/scripts/key_input.js
@@ -1,50 +1,90 @@
-export const keyInputs = (
-  window.addEventListener("keydown", function(event) {
-    if (event.defaultPrevented) {
-      return;
-    }
-   
-      switch(event.key) {
-        case 'a':
-          console.log('a down');
 
-          break;
-        case 's':
-          console.log('s down');
-          break;
-        case 'k':
-          console.log('k down');
-          break;
-        case 'l':
-          console.log('l down');
-          break;
-        default:
-          console.log('bingus down');
-      }
-    
-    
-},
-
-  window.addEventListener("keyup", function(event) {
-    if (event.defaultPrevented) {
-      return;
+class keyInputs {
+  constructor() {
+    this.keysDown = {
+      a: false,
+      s: false,
+      k: false,
+      l: false
     }
+    // this.keyDown();
+    // this.keyUp();
+    this.setKeyDown = this.setKeyDown.bind(this);
+    this.getKeyDown = this.getKeyDown.bind(this);
+  }
+  
+  keyDown() {
 
-    switch(event.key) {
-      case 'a':
-        console.log('a up');
-        break;
-      case 's':
-        console.log('s up');
-        break;
-      case 'k':
-        console.log('k up');
-        break;
-      case 'l':
-        console.log('l up');
-        break;
-      default:
-        console.log('bingus up')
-    }
-  })
-))
+    // window.addEventListener("keydown", function(event) {
+    //   // debugger;
+    //   if (event.defaultPrevented) {
+    //     return;
+    //   }
+     
+    //     switch(event.key) {
+    //       case 'a':
+    //         // setKeyDown('a');
+    //         // this.keysDown.a = true;
+    //         break;
+    //       case 's':
+    //         console.log('s down');
+    //         // keysDown.s = true;
+    //         break;
+    //       case 'k':
+    //         console.log('k down');
+    //         // keysDown.k = true;
+    //         break;
+    //       case 'l':
+    //         console.log('l down');
+    //         // keysDown.l = true;
+    //         break;
+    //       default:
+    //         console.log('key down');
+    //     }
+      
+      
+    // })
+  }
+
+  keyUp() {
+  //   window.addEventListener("keyup", function(event) {
+  //   if (event.defaultPrevented) {
+  //     return;
+  //   }
+
+  //   switch(event.key) {
+  //     case 'a':
+  //       // keysDown.a = false;
+  //       // console.log(`key: ${this.getKeyDown()}`)
+  //       // this.setKeyDown('a');
+  //       // console.log('a up');
+  //       break;
+  //     case 's':
+  //       // keysDown.s = false;
+  //       console.log('s up');
+  //       break;
+  //     case 'k':
+  //       // keysDown.k = false;
+  //       console.log('k up');
+  //       break;
+  //     case 'l':
+  //       // keysDown.l = false;
+  //       console.log('l up');
+  //       break;
+  //     default:
+  //       console.log('key up')
+  //   }
+  // })
+  }
+
+  getKeyDown() {
+    return this.keysDown;
+  }
+
+  setKeyDown(key) {
+    this.keysDown[key] = !this.keysDown[key];
+    // console.log(this.keysDown)
+  }
+}
+
+export default keyInputs;
